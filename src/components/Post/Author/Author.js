098bcda45be29bba18338de1df36aa626f6eef6b
Diego Nieto Cid <dnietoc@gmail.com>
2019-04-02 15:11:41 -0300

    [post] show one of twitter or github links

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -3,20 +3,24 @@ import { graphql, StaticQuery } from 'gatsby';
 import { getContactHref } from '../../../utils';
 import styles from './Author.module.scss';
 
+const capitalize = s => s && s[0].toUpperCase() + s.substring(1);
+
 export const PureAuthor = ({ data }) => {
   const { author } = data.site.siteMetadata;
-
+  const contact = Object.entries(author.contacts)
+    .filter(e => e[1] && e[1] !== '#')
+    .find(e => e[0] === 'twitter' || e[0] === 'github');
   return (
     <div className={styles['author']}>
       <p className={styles['author__bio']}>
         {author.bio}
         <a
-          className={styles['author__bio-twitter']}
-          href={getContactHref('twitter', author.contacts.twitter)}
+          className={styles[`author__bio-${contact[0]}`]}
+          href={getContactHref(contact[0], contact[1])}
           rel="noopener noreferrer"
           target="_blank"
         >
-          <strong>{author.name}</strong> on Twitter
+          <strong>{author.name}</strong> on {capitalize(contact[0])}
         </a>
       </p>
     </div>
@@ -34,6 +38,7 @@ export const Author = (props) => (
               bio
               contacts {       
                 twitter
+                github
               }
             }
           }
